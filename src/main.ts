77721b28324324b8377a823f45b1c43d129f75be
Natalia Popova <na.popova@qiwi.com>
2022-07-23 13:01:39 +0200

    feat: add docker

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-import { dirname, join } from 'path';
-import { readFile } from 'fs/promises';
-import { parse } from 'yaml';
-import { SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -15,10 +11,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  const rootDirname = dirname(__dirname);
-  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
-  const document = parse(DOC_API);
-  SwaggerModule.setup('doc', app, document);
+  console.log('Port running on: ', PORT);
+
   await app.listen(PORT);
 }
 bootstrap();
